69ceebcfb7c21f1777f06d18c9e8d1ad7a814756
sahalisro-blip <sahal.isro@gmail.com>
2026-05-05 11:29:43 +0000

    add: liflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import Liflet from "./components/Liflet";
+
 export default function App(){
   return <div>
     <h1 className="text-5xl text-red-500">hello there</h1>
+    <Liflet />
   </div>
 }
---
+++ src/components/Liflet.tsx
@@ -0,0 +1,40 @@
+// App.tsx
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+} from "react-leaflet";
+import { LatLngBounds } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
+
+const bounds = new LatLngBounds(
+  [8.0, 68.0], // southwest corner (India)
+  [35.5, 97.5] // northeast corner
+);
+
+function MapInitializer() {
+  const map = useMap();
+  useEffect(() => {
+    map.fitBounds(bounds);
+  }, [map]);
+  return null;
+}
+
+const Liflet: React.FC = () => {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer
+        bounds={bounds}
+        scrollWheelZoom={true}
+        markerZoomAnimation={true}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MapInitializer />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Liflet;
